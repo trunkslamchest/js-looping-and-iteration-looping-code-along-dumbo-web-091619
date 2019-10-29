1c41ecf09b806d315f2cfd8df4520a547b3b2dac
austin_smith <trunk9slamchest@gmail.com>
2019-10-29 09:39:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(array, string) {
+	let new_arr = []
+
+	for (let i = 0; i < array.length; i++) {
+		new_arr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+	}
+
+	console.log(new_arr)
+	return new_arr
+
+}
+
+// writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
+function countDown(n) {
+	let i = n
+	while (i >= 0) {
+		console.log(i)
+	i --
+	}
+}
+
+
+// countDown( 10 )
